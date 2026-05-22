9977a29beff538f11ca03f30ab7a325f856ef9c0
BurnBridges <Rinnegan107@yandex.ru>
2026-05-22 19:34:28 +0300

    clean my app 0.0.1

--- server/firebase.js
+++ server/firebase.js
@@ -1,12 +1,5 @@
 const admin = require("firebase-admin");
-
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FB_CLIENT_EMAIL,
-};
+const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
